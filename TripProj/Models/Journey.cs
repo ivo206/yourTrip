edcce9879a5cd891e69a4c56ede075be522cc950
Marijofill <boskovic.marijo.1988@gmail.com>
2017-04-06 19:39:18 +0100

    [yourTrip] Journey and User modals created

---
+++ TripProj/Models/Journey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripProj.Models
+{
+    public class Journey
+    {
+        public int Id { get; set; }
+        public LocationPoint StartPoint { get; set; }
+        public LocationPoint EndPoint { get; set; }
+        public TransportationEnum TransportationType { get; set; }
+        public LevelEnum Level { get; set; }
+        public int Rating { get; set; }
+    }
+
+    public enum TransportationEnum { Walk, Bike, Car, Public }
+    public enum LevelEnum { Novice, Intermediate, Advanced, Hardcore }
+}
---
+++ TripProj/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TripProj.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public enum Role { Admin, User, Guest }
+    }
+}
